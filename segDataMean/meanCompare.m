6e883b10a00966686d72c9119a14446e61c3f104
FingerStyleFinn <hfeng7@du.edu>
2021-08-09 20:44:38 +0800

    plot the intervention figure
    it shows that the mean value from intervention sessions also show the increasing
    however, I just thought about if the whole signal from one entire session shows uprising, then this plot means nothing. I have to check the plot from the original data.

--- segDataMean/meanCompare.m
+++ segDataMean/meanCompare.m
@@ -31,15 +31,31 @@ x = reshape(warmFile, 50,8);
 % x = warmFile;
 plot(x)
 
-%compare intervention means 
-% fileName = ...
-%     'E:\EDA_Process\C_Morlet_SVM\segDataMean\inter\filtermean_inter.mat';
-% 
-% disp('Loading File ...')
-% 
-% interFile = load(fileName);
-% interFile = interFile.saveClip3;
-% 
-% x = reshape(interFile, 4,6);
-% 
-% plot(x)
+%load mat file
+%compare warmup means
+fileName = ...
+    'E:\EDA_Process\C_Morlet_SVM\segDataMean\inter\norm_inter.mat';
+
+disp('Loading File ...')
+
+interFile = load(fileName);
+interFile = interFile.saveClip1;
+
+mean(interFile)
+max(interFile)
+min(abs(interFile))
+median(abs(interFile))
+
+% for i = 1:length(warmFile)
+%     if abs(warmFile(i)) > mean(warmFile)*10
+%         warmFile(i) = median(warmFile);
+%     end
+%     
+% end
+
+
+x = reshape(interFile, 48,6);
+% x = warmFile;
+plot(x)
+% plot the last subject's intervention data means
+plot(x(:, 6), '*')
